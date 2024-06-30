186b6f9dbbbce4558bbbb1ef38e2fd7c25d60c82
Elmar Mertens <em@elmarmertens.com>
2024-06-30 20:33:27 +0200

    dropped old project folders
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- kensingtonDataMatfiles/readme.md
+++
@@ -1,3 +0,0 @@
-please store mat files prepared by scripts in kensingtonDataUSSPF here
-
-see main readme.md file for further details
--- kensingtonET/etilt_obj.m
+++
@@ -1,7 +0,0 @@
-function [f_val,f_grad,f_hess] = etilt_obj(l,w,momdiff)
-%% Entropic tilting objective function, gradient, Hessian
-tmp=exp(l'*momdiff);
-f_val=tmp*w;  
-f_grad=tmp.*momdiff*w;
-f_hess=(tmp.*w').*momdiff*momdiff';
-end
